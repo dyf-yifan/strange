0f8626d7393b94ff3a176895db0bf15e4822c0ca
dingyifan <510040515@qq.com>
2020-11-12 14:17:50 +0800

    项目初始化

---
+++ music/App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	@import url("./common/css/free.css");
+	@import url("./common/css/iconfont.css");
+	@import url("./common/css/animate.css");
+	@import url("./common/css/myanimate.css");
+</style>
---
+++ music/pages/index/index.vue
@@ -0,0 +1,57 @@
+<template>
+	<view class="content">
+		<h2>music</h2>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<myaudio></myaudio>
+	</view>
+</template>
+
+<script>
+	import Myaudio from '@/components/myaudio.vue'
+	export default {
+		components:{
+			Myaudio
+		},
+		data() {
+			return {
+				title: '音频组件开发'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
